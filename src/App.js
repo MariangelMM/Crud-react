b319cd9244ff8abcde96967e394921d3b0a7e4c5
Mariangel Mora Marquez <mmoramar@emeal.nttdata.com>
2021-11-24 15:43:15 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,14 +11,20 @@ import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 
 export default function App() {
-  const [auth, setAuth] = useState({ name: "", isAuth: true });
+  const [auth, setAuth] = useState({ name: "", isAuth: false });
+
+  useEffect(() => {
+    // lectura de local storage para actualizar el estado auth en cada renderizado
+    const _auth = JSON.parse(localStorage.getItem("user"));
+    _auth && setAuth(_auth);
+  }, []);
 
   return (
     <Router>
       <div>
         <Switch>
           <Route path="/login">
-            {!auth.isAuth ? <Login /> : <Redirect to="/" />}
+            {!auth.isAuth ? <Login setAuth={setAuth} /> : <Redirect to="/" />}
           </Route>
           <Route path="/">
             {auth.isAuth ? <Home /> : <Redirect to="/login" />}
