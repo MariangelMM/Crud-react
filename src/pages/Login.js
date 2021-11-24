c93df21fb48b492205e05516ae69df6827d87205
kiswari10 <kiswari10@gmail.com>
2021-11-24 15:29:50 -0500

    ✨ add router in App

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Login = () => {
+    return (
+        <div>
+            Login
+            <br />
+            <Link to="/">Ir a Home</Link>
+        </div>
+    )
+}
