6b3cbd3a5c1d6d19e51c15ca7b38c1f31cdd41b9
Mariangel Mora Marquez <mmoramar@emeal.nttdata.com>
2021-11-30 16:35:07 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,16 +7,27 @@ import {
 } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
+// importa hoja de estilos de ant design
+import "antd/dist/antd.css";
+import "./App.css";
 
 export default function App() {
-  const [auth, setAuth] = useState({ nombre: "", isAuth: true });
+  const [auth, setAuth] = useState({ nombre: "", isAuth: false });
+
+
+  useEffect(() => {
+    // lectura de local storage para actualizar el estado auth en cada renderizado
+    const _auth = JSON.parse(localStorage.getItem("user"));
+    _auth && setAuth(_auth);
+  }, []);
+
 
   return (
     <Router>
       <div>
         <Switch>
           <Route path="/login">
-            {!auth.isAuth ? <Login /> : <Redirect to="/" />}
+            {!auth.isAuth ? <Login  setAuth={setAuth} /> : <Redirect to="/" />}
           </Route>
           <Route path="/">
             {auth.isAuth ? <Home /> : <Redirect to="/login" />}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,59 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { Form, Input, Button, message } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 
-export const Login = () => {
+
+export const Login = ({ setAuth }) => {
+  const history = useHistory();
+
+  const onFinish = (values) => {
+    localStorage.setItem("user", JSON.stringify({ ...values, isAuth: true }));
+    history.push("/");
+    setAuth({ ...values, isAuth: true });
+    message.success(`Bienvenid@ ${values.name}`);
+  };
+
   return (
     <div>
-      Login
-      <Link to="/">Ir a Home</Link>
+      <div className="login__container">
+      <div className="login__form">
+        <div className="login__title">Iniciar Sesión</div>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: "Por favor, ingrese su nombre",
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Ingrese su nombre"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Ingresar
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
     </div>
   );
 };
