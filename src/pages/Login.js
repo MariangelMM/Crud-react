300f161658aded27fe8ac18cd38b7914a241ad3d
Mariangel Mora Marquez <mmoramar@emeal.nttdata.com>
2021-12-03 10:32:53 -0500

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,67 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Form, Input, Button } from 'antd';
+import { useHistory } from "react-router-dom";
 
-export const Login = () => {
+export const Login = ({setAuth}) => {
+
+  let history = useHistory();
+
+  const onFinish = (values) => {
+    localStorage.setItem("user", JSON.stringify({...values, isAuth: true}));
+    history.push("/");
+    setAuth({...values, isAuth: true})
+
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div>
-      Login
-      <Link to="/">Ir a Home</Link>
+    <div className="login__container">
+      <div className="login__form">
+        <p className="login__title">Iniciar Sesión</p>
+        <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+    
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Nombre"
+        name="nombre"
+        rules={[
+          {
+            required: true,
+            message: 'Ingrese su nombre',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+     
+
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Ingresar
+        </Button>
+      </Form.Item>
+    </Form>
+      </div>
     </div>
   );
 };
