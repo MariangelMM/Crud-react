d06c531886b50c172dc25f6bc91bc2a105878335
Mariangel Mora Marquez <mmoramar@emeal.nttdata.com>
2021-12-03 13:32:20 -0500

    eliminar y editar

---
+++ src/components/home/AddUser.js
@@ -0,0 +1,77 @@
+import { Form, Input, Button } from 'antd';
+
+export const AddUser = ({addUser}) => {
+  const onFinish = (values) => {
+    addUser(values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      layout="inline"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Nombres"
+        name="nombres"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Apellidos"
+        name="apellidos"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Puesto"
+        name="puesto"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+     
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Agregar
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
--- src/components/home/TablaUser.js
+++ src/components/home/TablaUser.js
@@ -1,84 +1,98 @@
 import React, { useState, useEffect } from "react";
 import { Table, Space, Button, Popconfirm, message } from "antd";
-import { get, deleteUser } from "../../api/config";
-
-
+import { get, deleteUser, post} from "../../api/config";
+import { AddUser } from "./AddUser";
 
 export const TablaUser = () => {
+  const columns = [
+    {
+      title: "Nombres",
+      dataIndex: "nombres",
+      key: "nombres",
+    },
+    {
+      title: "Apellidos",
+      dataIndex: "apellidos",
+      key: "apellidos",
+    },
+    {
+      title: "Puesto",
+      dataIndex: "puesto",
+      key: "puesto",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text, record) => (
+        <Space size="middle">
+          <Popconfirm
+            title="¿Estas seguro de eliminar?"
+            onConfirm={() => confirm(record.id)}
+            onCancel={cancel}
+            okText="Si"
+            cancelText="No"
+          >
+            <Button type="primary" danger>
+              Eliminar
+            </Button>
+          </Popconfirm>
 
-    const columns = [
-        {
-          title: "Nombres",
-          dataIndex: "nombres",
-          key: "nombres",
-        },
-        {
-          title: "Apellidos",
-          dataIndex: "apellidos",
-          key: "apellidos",
-        },
-        {
-          title: "Puesto",
-          dataIndex: "puesto",
-          key: "puesto",
-        },
-        {
-          title: "Action",
-          key: "action",
-          render: (text, record) => (
-            <Space size="middle">
-              <Popconfirm
-                title="¿Estas seguro de eliminar?"
-                onConfirm={() => confirm(record.id)}
-                onCancel={cancel}
-                okText="Si"
-                cancelText="No"
-              >
-                <Button type="primary" danger>
-                  Eliminar
-                </Button>
-              </Popconfirm>
-            </Space>
-          ),
-        },
-      ];
-      
-      const confirm = async (id) => {
-          try{
-              const response = await deleteUser(id)
-              if(response.status === 200) {
-                  message.success("usuario eliminado");
-              }
-              setUpdate(false)
-          }
-          catch (error){
-              console.log(error)
-          }
-        
-      
-       
+          <Popconfirm
+            title="¿Estas seguro de eliminar?"
+            onConfirm={() => confirm(record.id)}
+            onCancel={cancel}
+            okText="Si"
+            cancelText="No"
+          >
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ];
+
+  const confirm = async (id) => {
+    try {
+      const response = await deleteUser(id);
+      if (response.status === 200) {
+        message.success("usuario eliminado");
       }
-      
-      function cancel(e) {
-        console.log(e);
-        message.error("Click on No");
-      }
+      getUsers();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-
-
+  function cancel(e) {
+    console.log(e);
+    message.error("Click on No");
+  }
 
   const [data, setData] = useState("");
-  const [update, setUpdate] = useState(true)
 
   const getUsers = async () => {
     const response = await get();
     setData(response);
   };
 
+
+const addUser = async (user) => {
+    const response = await post(user);
+    if (response.status === 201) {
+      message.success("usuario agregado");
+    }
+    getUsers();
+}
+
+
   useEffect(() => {
     getUsers();
+  }, []);
 
-  }, [update]);
-
-  return <Table dataSource={data} columns={columns} />;
+  return (
+    <div>
+    <AddUser addUser={addUser}/>
+      <br/>
+      <Table dataSource={data} columns={columns} />
+    </div>
+  );
 };
